3336ad02a6211997b539094e5eac6746622fe652
Tanner Quigley <TannerQuigley@me.com>
2018-05-21 10:55:28 -0700

    handle port number

--- lib/web/grove_pi_message.ex
+++ lib/web/grove_pi_message.ex
@@ -9,7 +9,24 @@ defmodule Web.GrovePiMessage do
   end
 
   def set_component_value_message(list) do
-    Blockytalky.GrovePi.set_component_value(:D2, 1)
+    IO.puts("MESSAGE")
+
+    port = list["options"]["port"]
+    IO.puts(list["options"]["port"])
+
+    case port do
+	2 -> Blockytalky.GrovePi.set_component_value(:D2, 1)
+	3 -> Blockytalky.GrovePi.set_component_value(:D3, 1)
+	4 -> Blockytalky.GrovePi.set_component_value(:D4, 1)
+	5 -> Blockytalky.GrovePi.set_component_value(:D5, 1)
+	6 -> Blockytalky.GrovePi.set_component_value(:D6, 1)
+	7 -> Blockytalky.GrovePi.set_component_value(:D7, 1)
+	8 -> Blockytalky.GrovePi.set_component_value(:D8, 1)
+	_ -> IO.puts("A non digital port was selected")
+    end
+
+
+    
   end
 
   def get_component_value_message(list) do
