3f2254b633fad23b7d3171fc1ab2f8193ca37e89
Tanner Quigley <TannerQuigley@me.com>
2018-05-21 12:54:25 -0700

    Basic set_component_type handling

--- lib/web/grove_pi_message.ex
+++ lib/web/grove_pi_message.ex
@@ -6,13 +6,21 @@ defmodule Web.GrovePiMessage do
   
   def set_component_type_message(list) do
     IO.puts("message module got set type")
+    
+    port = list["options"]["port"]
+    type = list["deviceType"]
+
+    IO.puts(port)
+    IO.puts(type)
+
+    Blockytalky.GrovePi.set_component_type(:D2,type)
+
   end
 
   def set_component_value_message(list) do
     IO.puts("MESSAGE")
 
     port = list["options"]["port"]
-    IO.puts(list["options"]["port"])
 
     case port do
 	2 -> Blockytalky.GrovePi.set_component_value(:D2, 1)
